feec3f92783958bffa4c024d8243891ec1d4da0d
daudi13 <davouma13@gmail.com>
2023-11-15 12:45:00 +0300

    Add metaImage

--- app/meet/page.jsx
+++ app/meet/page.jsx
@@ -8,13 +8,14 @@ import group113 from "../assets/Group111.png";
 import Link from 'next/link';
 import Image from 'next/image'
 import { featuredProfiles, devTools, cards } from '../data/data';
-import { images } from '@/next.config';
+import heroImage from "../assets/HeroImageCluster.png";
+
 
 export const metadata = {
   title: 'Tana | Our Talent',
   description: "Elevate your business with our premier talent network! Connect with skilled QA engineers, customer technical support specialists, and data analytics professionals through our platform. Streamline your operations, enhance product quality, and gain valuable insights with our curated pool of experts. Explore a seamless hiring experience and elevate your company's performance with our comprehensive workforce solutions. Partner with us for unparalleled access to top-tier professionals in QA, customer support, and data analytics. Unlock the potential of your business with our talent-driven solutions today.",
   type: "website",
-  images: "../assets/HeroImageCluster.png",
+  images: [heroImage],
   url: "https://tanatech.vercel.app/meet",
 }
 
